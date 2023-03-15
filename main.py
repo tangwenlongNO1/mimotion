0ab9a6534ceee46fe454961a4cd167792cbf7d23
tangwenlongNO1 <106858241+tangwenlongNO1@users.noreply.github.com>
2023-03-15 15:07:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -78,8 +78,8 @@ def getBeijinTime():
         max_ratio = math.ceil(int(hour) / 3)
         min_1 = 3500 * min_ratio
         max_1 = 3500 * max_ratio
-        min_1 = int(K * min_1)
-        max_1 = int(K * max_1)
+        min_1 = 10000
+        max_1 = 12000
     else:
         print("获取北京时间失败")
         return
