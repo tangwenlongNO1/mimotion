477dd649f1eac0e2f8243df71b15d91ecfd2eae7
tangwenlongNO1 <106858241+tangwenlongNO1@users.noreply.github.com>
2023-04-05 13:35:31 +0800

    Create node.js

---
+++ node.js
@@ -0,0 +1,17 @@
+const axios = require('axios');
+
+async function getUpdateInfo() {
+  console.log('Checking for updates...');
+  const response = await axios.get('https://api.github.com/repos/Fndroid/clash_for_windows_pkg/releases/latest');
+
+  return {
+    name: response.data.name,
+    version: response.data.tag_name,
+    url: response.data.assets[0].browser_download_url,
+    changelog: response.data.body,
+  };
+}
+
+getUpdateInfo().then(info => {
+  console.log(info);
+});
